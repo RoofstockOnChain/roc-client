4f412c2e0f2051d0b8291f8b8793a213709f80ed
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2023-05-30 13:32:35 -0500

    Add basic layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { TopBar } from './layout/TopBar';
+import {Route, Routes} from "react-router-dom";
+import { List } from './pages/List';
+import { Mint } from './pages/Mint';
+import { Burn } from './pages/Burn';
+import { Administration } from './pages/Administration';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <TopBar />
+      <Routes>
+        <Route index element={<List />} />
+        <Route path="mint" element={<Mint />} />
+        <Route path="mint" element={<Burn />} />
+        <Route path="admin" element={<Administration />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/layout/TopBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AppBar, Box, Button, Link, styled, Toolbar } from '@mui/material';
+
+const StyledButton = styled(Button)`
+  color: #fff;
+`;
+
+export const TopBar = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Link href="/">
+          <img
+            src="/images/roofstock-onchain-logo.svg"
+            alt="Roofstock onChain Logo"
+          />
+        </Link>
+        <Box>
+          <StyledButton href="/mint">Mint</StyledButton>
+          <StyledButton href="/burn">Burn</StyledButton>
+          <StyledButton href="/admin">Administration</StyledButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
