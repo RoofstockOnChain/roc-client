ea773f1839a9b0c6525b599f033a9f3690185a4f
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2023-06-01 10:31:34 -0500

    Add site footer (#4)
    * Add site footer
    
    * Add top bar link divider

--- src/layout/TopBar.tsx
+++ src/layout/TopBar.tsx
@@ -1,31 +1,67 @@
 import React from 'react';
-import { AppBar, Box, Button, Link, styled, Toolbar } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Link,
+  Stack,
+  styled,
+  Toolbar,
+} from '@mui/material';
 
-const StyledBox = styled(Box)`
-  text-align: end;
-  width: 100%;
+const StyledAppBar = styled(AppBar)`
+  background-color: #151920;
+  justify-content: center;
+  min-height: 100px;
+`;
+
+const StyledImg = styled('img')`
+  padding-left: 1rem;
+  padding-right: 1rem;
+`;
+
+const DividerStyled = styled(Divider)`
+  background-color: #fff;
 `;
 
 const StyledButton = styled(Button)`
   color: #fff;
+  text-transform: none;
 `;
 
 export const TopBar = () => {
   return (
-    <AppBar position="static">
+    <StyledAppBar position="static">
       <Toolbar>
         <Link href="/">
-          <img
+          <StyledImg
             src="/images/roofstock-onchain-logo.svg"
             alt="Roofstock onChain Logo"
           />
         </Link>
-        <StyledBox>
+        <Stack
+          display="flex"
+          columnGap="1rem"
+          direction="row"
+          divider={
+            <DividerStyled
+              flexItem
+              light
+              orientation="vertical"
+              sx={{ alignSelf: 'center', height: '1rem' }}
+            />
+          }
+        >
+          <StyledButton href="/properties">Properties</StyledButton>
           <StyledButton href="/mint">Mint</StyledButton>
           <StyledButton href="/burn">Burn</StyledButton>
           <StyledButton href="/admin">Administration</StyledButton>
-        </StyledBox>
+        </Stack>
+        <Box textAlign="end" width="100%">
+          <StyledButton>Connect Wallet</StyledButton>
+        </Box>
       </Toolbar>
-    </AppBar>
+    </StyledAppBar>
   );
 };
