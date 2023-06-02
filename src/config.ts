e1ea1a934517ce00c099cdf6e3d5a9b40624e23d
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2023-06-02 13:17:01 -0500

    Refine footer

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 export const config: RocClientConfig = {
   verifiedSmartContract: process.env.REACT_APP_VERIFIED_SMART_CONTRACT!,
+  marketplaceUrl: process.env.REACT_APP_MARKETPLACE_URL!,
 };
 
 type RocClientConfig = {
   verifiedSmartContract: string;
+  marketplaceUrl: string;
 };
--- src/layout/TopBar.tsx
+++ src/layout/TopBar.tsx
@@ -3,22 +3,19 @@ import {
   AppBar,
   Box,
   Button,
+  Container,
   Divider,
   Link,
   Stack,
   styled,
   Toolbar,
 } from '@mui/material';
+import { config } from '../config';
 
 const StyledAppBar = styled(AppBar)`
   background-color: #151920;
   justify-content: center;
   min-height: 100px;
-`;
-
-const StyledImg = styled('img')`
-  padding-left: 1rem;
-  padding-right: 1rem;
 `;
 
 const DividerStyled = styled(Divider)`
@@ -28,40 +25,48 @@ const DividerStyled = styled(Divider)`
 const StyledButton = styled(Button)`
   color: #fff;
   text-transform: none;
+  white-space: nowrap;
 `;
 
 export const TopBar = () => {
+  const { marketplaceUrl } = config;
+
   return (
     <StyledAppBar position="static">
-      <Toolbar>
-        <Link href="/">
-          <StyledImg
-            src="/images/roofstock-onchain-logo.svg"
-            alt="Roofstock onChain Logo"
-          />
-        </Link>
-        <Stack
-          display="flex"
-          columnGap="1rem"
-          direction="row"
-          divider={
-            <DividerStyled
-              flexItem
-              light
-              orientation="vertical"
-              sx={{ alignSelf: 'center', height: '1rem' }}
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Link href="/">
+            <img
+              src="/images/roofstock-onchain-logo.svg"
+              alt="Roofstock onChain Logo"
             />
-          }
-        >
-          <StyledButton href="/properties">Properties</StyledButton>
-          <StyledButton href="/mint">Mint</StyledButton>
-          <StyledButton href="/burn">Burn</StyledButton>
-          <StyledButton href="/admin">Administration</StyledButton>
-        </Stack>
-        <Box textAlign="end" width="100%">
-          <StyledButton>Connect Wallet</StyledButton>
-        </Box>
-      </Toolbar>
+          </Link>
+          <Stack
+            display="flex"
+            columnGap="1rem"
+            direction="row"
+            divider={
+              <DividerStyled
+                flexItem
+                light
+                orientation="vertical"
+                sx={{ alignSelf: 'center', height: '1rem' }}
+              />
+            }
+            paddingLeft="2rem"
+            paddingRight="2rem"
+          >
+            <StyledButton href="/properties">View Homes onChain</StyledButton>
+            <StyledButton href={marketplaceUrl}>
+              Buy on Marketplace
+            </StyledButton>
+            <StyledButton>Learn</StyledButton>
+          </Stack>
+          <Box textAlign="end" width="100%">
+            <StyledButton>Connect Wallet</StyledButton>
+          </Box>
+        </Toolbar>
+      </Container>
     </StyledAppBar>
   );
 };
