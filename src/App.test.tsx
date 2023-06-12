4cb0eecc58cf741d50f25a1489785df8e1218012
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2023-06-12 12:33:15 -0500

    Fix broken test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,16 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { RocThemeProvider } from './providers/RocThemeProvider';
+import { HelmetProvider } from 'react-helmet-async';
 
 test('renders learn react link', () => {
   render(
     <BrowserRouter>
-      <RocThemeProvider>
-        <App />
-      </RocThemeProvider>
+      <HelmetProvider>
+        <RocThemeProvider>
+          <App />
+        </RocThemeProvider>
+      </HelmetProvider>
     </BrowserRouter>
   );
   const logoElement = screen.getByAltText('Roofstock onChain Logo');
