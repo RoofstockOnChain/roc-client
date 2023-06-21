39beb8293350b1f3efea0c0c77297435f677e376
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2023-06-21 15:05:02 -0500

    Add page path to GA script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -59,7 +59,9 @@ const Document: FC = () => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-MX3VKB200C');
+            gtag('config', 'G-MX3VKB200C', {
+              page_path: window.location.pathname,
+            });
           `,
           }}
         />
