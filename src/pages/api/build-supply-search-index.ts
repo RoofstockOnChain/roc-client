7d3ec2f9db0edfbd717cb1cbd72a040bdaef22b1
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2023-09-14 14:29:55 -0500

    Add more fields to the search index

--- src/pages/api/build-supply-search-index.ts
+++ src/pages/api/build-supply-search-index.ts
@@ -14,6 +14,17 @@ const handler = async (request: NextApiRequest, response: NextApiResponse) => {
     const listings = await getListings();
     const searchableListings = listings.map((x) => ({
       id: x.mlsListingId,
+      address1: x.address1,
+      city: x.city,
+      state: x.state,
+      zip: x.zip,
+      mainImageUrl: x.mainImageUrl,
+      bedrooms: x.bedrooms,
+      bathrooms: x.bathrooms,
+      homeSizeSquareFoot: x.homeSizeSquareFoot,
+      lotSizeSquareFoot: x.lotSizeSquareFoot,
+      yearBuilt: x.yearBuilt,
+      listingPrice: x.listingPrice,
       listingDescription: `Id: ${x.mlsListingId}. This property in the Columbia, SC market and the address is ${x.address1}. It is listed for ${x.listingPrice}. This property has ${x.bedrooms} bedrooms, ${x.bathrooms} bathrooms, and was built in ${x.yearBuilt}`,
     }));
     await searchClient.uploadDocuments(searchableListings);
