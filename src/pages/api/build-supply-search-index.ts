9b55a850a2f162b50b8c0ea91c8723a24a2076e4
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2023-10-12 12:33:35 -0500

    Set max duration for build supply search index

--- src/pages/api/build-supply-search-index.ts
+++ src/pages/api/build-supply-search-index.ts
@@ -1,15 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { SearchClient } from '@azure/search-documents';
 import { AzureKeyCredential } from '@azure/openai';
-import { config } from '@/config';
+import { config as rocConfig } from '@/config';
 import { getAllListings } from '@/services/ListingService';
+
+export const config = {
+  maxDuration: 300,
+};
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   try {
     const searchClient = new SearchClient(
-      config.azureSearchServiceEndpoint,
-      config.azureSearchServiceIndex,
-      new AzureKeyCredential(config.azureSearchServiceApiKey)
+      rocConfig.azureSearchServiceEndpoint,
+      rocConfig.azureSearchServiceIndex,
+      new AzureKeyCredential(rocConfig.azureSearchServiceApiKey)
     );
     const listings = await getAllListings();
     const searchableListings = listings.map((x) => ({
@@ -26,7 +30,7 @@ const handler = async (request: NextApiRequest, response: NextApiResponse) => {
       yearBuilt: x.yearBuilt,
       listingPrice: x.listingPrice,
       market: x.market,
-      listingDescription: `Id: ${x.mlsListingId}. This property in the ${x.market} market and the address is ${x.address1}. It is listed for ${x.listingPrice}. This property has ${x.bedrooms} bedrooms, ${x.bathrooms} bathrooms, and was built in ${x.yearBuilt}`,
+      listingDescription: `Id: ${x.mlsListingId}. This property in the ${x.market} market and the address is ${x.address1}. It is listed for ${x.listingPrice}. This property has ${x.bedrooms} bedrooms, ${x.bathrooms} bathrooms, and was built in ${x.yearBuilt}. The square footage of the home is ${x.homeSizeSquareFoot}.`,
     }));
     await searchClient.uploadDocuments(searchableListings);
     response.status(200).json({
